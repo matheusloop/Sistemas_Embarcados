9b0d3604bec74ca08f264391b43b5c0090f4d9f9
matheusloop <65179077+matheusloop@users.noreply.github.com>
2025-07-23 09:50:14 -0300

    Heatting Coolling System V2.0
    Mudança no Timer1 para o Timer0

--- Retrofit_8bits_ATmega328p/Heating_Cooling_System/Heating_Cooling_System_Lib/heating_cooling_system.c
+++ Retrofit_8bits_ATmega328p/Heating_Cooling_System/Heating_Cooling_System_Lib/heating_cooling_system.c
@@ -2,29 +2,29 @@
 
 void setupHeatingCoolingSystem(void){
    
-    DDRB |= ((1 << 1) | (1 << 2));  // Configura os pinos PB1 (OC1A) e PB2 (OC1B) como saída
+    DDRD |= ((1 << 5) | (1 << 6));  // Configura os pinos PD5 (OC0B) e PD6 (OC0A) como saída
 
     // Configura Timer1 para modo PWM rápido
-    // COM1A1:0 = 10 (não-invertido para OC1A)
-    // COM1B1:0 = 10 (não-invertido para OC1B)
+    // COM1A1:0 = 10 (não-invertido para OC0A)
+    // COM1B1:0 = 10 (não-invertido para OC0B)
     // WGM11:0 = 11 (modo 14 - Fast PWM com 0xFF como TOP)
-    TCCR1A = 0b10100011;
+    TCCR0A = 0b10100011;
 
     // WGM13:2 = 10 (com os bits anteriores, forma modo 14)
     // CS10 = 1 (sem prescaler, clk/1)
-    TCCR1B = 0b00000001;
+    TCCR0B = 0b00000011; //T = (256 * 64)/16MHz = ~1 ms 
 
     // Duty cycle inicial de 0% para ambos os canais
-    OCR1A = 0; // Aquecedor
-    OCR1B = 0; // Resfriador
+    OCR0A = 0; // Aquecedor
+    OCR0B = 0; // Resfriador
 }
 
 void setHeaterDutyCycle(uint8_t heaterDutyCycle){
     // Ajusta OCR1A proporcionalmente ao valor percentual
-    OCR1A = (1023 / 100) * heaterDutyCycle;
+    OCR0A = (255 / 100) * heaterDutyCycle;
 }
 
 void setCoolerDutyCycle(uint8_t coolerDutyCycle){
     // Ajusta OCR1B proporcionalmente ao valor percentual
-    OCR1B = (1023 / 100) * coolerDutyCycle;
+    OCR0B = (255 / 100) * coolerDutyCycle;
 }
