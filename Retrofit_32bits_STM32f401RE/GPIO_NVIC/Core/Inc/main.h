7f1e647b5a3b37d103010efc18301c036047bf0a
matheusloop <marcosraila@gmail.com>
2025-09-05 15:51:04 -0300

    Pratica GPIO_NVIC

---
+++ Retrofit_32bits_STM32f401RE/GPIO_NVIC/Core/Inc/main.h
@@ -0,0 +1,97 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_hal.h"
+
+#include "stm32c0xx_nucleo.h"
+#include <stdio.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RCC_OSCX_IN_Pin GPIO_PIN_14
+#define RCC_OSCX_IN_GPIO_Port GPIOC
+#define RCC_OSCX_OUT_Pin GPIO_PIN_15
+#define RCC_OSCX_OUT_GPIO_Port GPIOC
+#define RCC_OSC_IN_Pin GPIO_PIN_0
+#define RCC_OSC_IN_GPIO_Port GPIOF
+#define RCC_OSC_OUT_Pin GPIO_PIN_1
+#define RCC_OSC_OUT_GPIO_Port GPIOF
+#define Button_A_Pin GPIO_PIN_10
+#define Button_A_GPIO_Port GPIOB
+#define Button_A_EXTI_IRQn EXTI4_15_IRQn
+#define DEBUG_SWDIO_Pin GPIO_PIN_13
+#define DEBUG_SWDIO_GPIO_Port GPIOA
+#define DEBUG_SWCLK_Pin GPIO_PIN_14
+#define DEBUG_SWCLK_GPIO_Port GPIOA
+#define LED_Correct_Pin GPIO_PIN_0
+#define LED_Correct_GPIO_Port GPIOD
+#define LED_Wrong_Pin GPIO_PIN_1
+#define LED_Wrong_GPIO_Port GPIOD
+#define Button_B_Pin GPIO_PIN_4
+#define Button_B_GPIO_Port GPIOB
+#define Button_B_EXTI_IRQn EXTI4_15_IRQn
+#define Button_C_Pin GPIO_PIN_5
+#define Button_C_GPIO_Port GPIOB
+#define Button_C_EXTI_IRQn EXTI4_15_IRQn
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
